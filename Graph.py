06f6407d46c5277638c23c71a84c135b0ca93d3e
eletricmano <58055884+eletricmano@users.noreply.github.com>
2022-03-31 18:25:57 -0300

    Implemented directed/undirected logic

--- Graph.py
+++ Graph.py
@@ -1,9 +1,14 @@
 class Graph:
-    def __init__(self, vertices):
+    def __init__(self, vertices: int, directed: bool):
         self.vertices = [Vertex(u) for u in range(vertices)]
+        self.directed = directed
 
     def add_edge(self, u, v):
-        self.vertices[u].adj.append(self.vertices[v])
+        if self.directed:
+            self.vertices[u].adj.append(self.vertices[v])
+        else:
+            self.vertices[u].adj.append(self.vertices[v])
+            self.vertices[v].adj.append(self.vertices[u])
 
     def __str__(self):
         s = ""
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from Graph import *
 
-g = Graph(7)
+g = Graph(vertices=7, directed=True)
 g.add_edge(0, 1)
 g.add_edge(0, 2)
 g.add_edge(1, 3)
